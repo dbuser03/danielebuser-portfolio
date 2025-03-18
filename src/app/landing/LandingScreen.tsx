439db59c05f566fbad9bca1bcee185a9476700ff
dbuser03 <d.buser@campus.unimib.it>
2025-03-18 15:34:09 +0100

    feat(app): Simplify LandingScreen component
    Simplify LandingScreen component and move cursor logic to Home component.
    
    Changelog: feature

--- src/app/landing/LandingScreen.tsx
+++ src/app/landing/LandingScreen.tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import { useColumnCount } from "@/hooks/core/useColumnCount";
-import { useDeviceDetection } from "@/hooks/core/useDeviceDetection";
 import ColumnGrid from "@/components/core/ColumnGrid";
-import CustomCursor from "@/components/core/CustomCursor";
-import { LandingScreenProps } from "@/types/landing/landingTypes";
-import { LABEL_CLICK, LABEL_DEFAULT, LABEL_LOADING } from "@/constants/cursor";
 
-const LandingScreen = ({ readyForClick, hideLabel }: LandingScreenProps) => {
+const LandingScreen = () => {
   const columnCount = useColumnCount();
-  const { isTouchDevice } = useDeviceDetection();
-
-  const getCursorLabel = () => {
-    if (hideLabel) return LABEL_DEFAULT;
-    return readyForClick ? LABEL_CLICK : LABEL_LOADING;
-  };
 
   return (
     <div className="relative h-[100vh] flex-shrink-0">
       <ColumnGrid columnCount={columnCount} showBorder={false} opacity={1} />
-      {!isTouchDevice && <CustomCursor label={getCursorLabel()} />}
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,18 @@
 import { LandingScreen } from "@/app/landing";
 import { Footer, Header } from "@/components/layout";
 import { useHomePageLogic } from "@/hooks/useHomePageLogic";
+import CustomCursor from "@/components/core/CustomCursor";
+import { useDeviceDetection } from "@/hooks/core/useDeviceDetection";
+import { LABEL_CLICK, LABEL_DEFAULT, LABEL_LOADING } from "@/constants/cursor";
 
 export default function Home() {
   const { handleClick, readyForClick, hideLabel, loaded } = useHomePageLogic();
+  const { isTouchDevice } = useDeviceDetection();
+
+  const getCursorLabel = () => {
+    if (hideLabel) return LABEL_DEFAULT;
+    return readyForClick ? LABEL_CLICK : LABEL_LOADING;
+  };
 
   return (
     <div
@@ -13,8 +22,9 @@ export default function Home() {
       onClick={handleClick}
     >
       <Header loaded={loaded} />
-      <LandingScreen readyForClick={readyForClick} hideLabel={hideLabel} />
+      <LandingScreen />
       <Footer loaded={loaded} />
+      {!isTouchDevice && <CustomCursor label={getCursorLabel()} />}
     </div>
   );
 }
