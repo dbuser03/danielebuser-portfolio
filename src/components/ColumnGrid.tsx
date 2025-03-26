ebf53424c05759d0b415316e2feb9bdeb5124c5e
dbuser03 <buserdaniele@gmail.com>
2025-03-26 09:58:21 +0100

    feat(app): Move GRID_STYLES to constants
    Changelog: feature

--- src/components/ColumnGrid.tsx
+++ src/components/ColumnGrid.tsx
@@ -1,21 +1,7 @@
 import React from "react";
 import { ColumnGridProps } from "@/types/columnGridS";
 import { useColumnGrid } from "@/hooks/useWindowSize";
-
-const GRID_STYLES = {
-  CONTAINER: [
-    "pointer-events-none",
-    "absolute inset-0",
-    "-z-1",
-    "grid h-full w-full",
-    "grid-cols-1",
-    "gap-x-4 px-4 md:px-6",
-  ].join(" "),
-  COLUMN: {
-    BASE: "h-full bg-[var(--columns)]",
-    BORDER: "border border-[var(--foreground)]",
-  },
-};
+import { GRID_STYLES } from "@/constants/columnGrid";
 
 const ColumnGrid: React.FC<ColumnGridProps> = React.memo(
   ({ showBorder = false, opacity = 1, className = "" }) => {
--- src/constants/columnGrid.ts
+++ src/constants/columnGrid.ts
@@ -17,3 +17,18 @@ export const COLUMNS_BY_BREAKPOINT = ColumnsByBreakpointSchema.parse({
 });
 
 export const LARGE_SCREEN_WIDTH = '64vw';
+
+export const GRID_STYLES = {
+  CONTAINER: [
+    "pointer-events-none",
+    "absolute inset-0",
+    "-z-1",
+    "grid h-full w-full",
+    "grid-cols-1",
+    "gap-x-4 px-4 md:px-6",
+  ].join(" "),
+  COLUMN: {
+    BASE: "h-full bg-[var(--columns)]",
+    BORDER: "border border-[var(--foreground)]",
+  },
+};
