491b099e5af274c71aca3d603ab6afafc73ae524
dbuser03 <buserdaniele@gmail.com>
2025-05-15 10:05:13 +0200

    feat(app): Enhance Home and Profile components
    Enhance Home and Profile components with improved layout and user-select styles.
    
    Changelog: feature

--- src/components/about/Profile.tsx
+++ src/components/about/Profile.tsx
@@ -3,10 +3,23 @@ import { Columns } from "@/components/columns";
 
 export const Profile: React.FC = () => {
   return (
-    <section className="relative flex h-full w-full flex-col items-center justify-center">
-      <Columns maxColumns={12} alternative={true}/>
-      <h1>Profile</h1>
-    </section>
+    <>
+      <section className="relative flex h-screen w-full flex-col items-start justify-center">
+        <Columns maxColumns={12} alternative={true} />
+        <h1 className="max-w-6xl mx-4 md:mx-6 text-5xl md:text-7xl text-[var(--background)]">
+          I'm a <span className="font-bold">computer science student</span>{" "}
+          based in Milan willing to become an amazing{" "}
+          <span className="font-bold">creative developer</span>.
+        </h1>
+      </section>
+
+      <section className="relative flex h-screen w-full flex-col items-start justify-center">
+        <Columns maxColumns={12} alternative={true} />
+        <div className="max-w-8xl mx-6 text-[var(--background)]">
+          {/* Add your content here */}
+        </div>
+      </section>
+    </>
   );
 };
 
